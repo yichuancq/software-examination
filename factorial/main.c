5cfc5c1ff12064949c3d1ad21178ff752d21356a
yichuan <1012027293@qq.com>
2022-04-20 01:56:55 +0800

    斐波那契数列

---
+++ factorial/main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  factorial
+//
+//  Created by yichuan on 2022/4/20.
+//
+
+#include <stdio.h>
+//斐波那契数列
+int fibonaci(int i)
+{
+  if(i == 0)
+  {
+     return 0;
+  }
+  if(i == 1)
+  {
+     return 1;
+  }
+  return fibonaci(i-1) + fibonaci(i-2);
+}
+
+int  main()
+{
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+       printf("%d\t\n", fibonaci(i));
+    }
+    return 0;
+}
